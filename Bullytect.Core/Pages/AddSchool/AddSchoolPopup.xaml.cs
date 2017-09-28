c5e734b0c3aefff78c2da7a924aeb2dca39081fa
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2017-09-28 14:58:26 +0200

    feat(page): Add School Page

---
+++ Bullytect.Core/Pages/AddSchool/AddSchoolPopup.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Bullytect.Core.Pages.AddSchool
+{
+    public partial class AddSchoolPopup : ContentView
+    {
+        public AddSchoolPopup()
+        {
+            InitializeComponent();
+        }
+    }
+}
