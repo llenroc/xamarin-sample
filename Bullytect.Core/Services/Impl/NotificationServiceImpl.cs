9154851d3e6590a8d18c93f22e6b3009660012d2
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2017-10-27 14:31:35 +0200

    fix(app): solve issues.

--- Bullytect.Core/Services/Impl/NotificationServiceImpl.cs
+++ Bullytect.Core/Services/Impl/NotificationServiceImpl.cs
@@ -1,10 +1,63 @@
 ﻿using System;
+using System.Diagnostics;
+using Bullytect.Core.Config;
+using Bullytect.Core.Models.Domain;
+using Bullytect.Core.ViewModels;
+using MvvmCross.Core.Navigation;
+using MvvmCross.Platform;
+using Plugin.DeviceInfo;
+using Plugin.FirebasePushNotification;
+
 namespace Bullytect.Core.Services.Impl
 {
-    public class NotificationServiceImpl
+    public class NotificationServiceImpl : INotificationService
     {
-        public NotificationServiceImpl()
+
+        readonly IDeviceGroupsService _deviceGroupsService;
+
+        public NotificationServiceImpl(IDeviceGroupsService deviceGroupsService)
         {
+            _deviceGroupsService = deviceGroupsService;
+
+            initNotificationHandlers();
         }
+
+        protected void initNotificationHandlers()
+        {
+
+            CrossFirebasePushNotification.Current.OnTokenRefresh += (s, p) =>
+            {
+                Debug.WriteLine($"TOKEN REC: {p.Token}");
+            };
+
+            CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) => {
+                System.Diagnostics.Debug.WriteLine("Received");
+                System.Diagnostics.Debug.WriteLine(p.Data);
+            };
+
+			CrossFirebasePushNotification.Current.OnNotificationOpened += (s, p) =>
+			{
+				System.Diagnostics.Debug.WriteLine("Opened");
+				if (Settings.AccessToken != null)
+				{
+					Mvx.Resolve<IMvxNavigationService>()?.Navigate<AlertsViewModel>();
+				}
+
+			};
+        }
+
+        public IObservable<DeviceEntity> subscribeDevice()
+        {
+			Debug.WriteLine(String.Format("Device Id: {0}", CrossDeviceInfo.Current.Id));
+			Debug.WriteLine(String.Format("FCM Token: {0}", CrossFirebasePushNotification.Current.Token));
+			return _deviceGroupsService.saveDevice(CrossDeviceInfo.Current.Id, CrossFirebasePushNotification.Current.Token);
+        }
+
+        public IObservable<string> unsubscribeDevice()
+        {
+            Debug.WriteLine(String.Format("Unsubscribe Device : {0}", CrossDeviceInfo.Current.Id));
+            return _deviceGroupsService.Delete(CrossDeviceInfo.Current.Id);
+        }
+
     }
 }
